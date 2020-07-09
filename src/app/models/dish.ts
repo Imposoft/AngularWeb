7cf75b84dd93bcea4861b094da9f9ec57c3689a9
apatagg <31703613+apatagg@users.noreply.github.com>
2020-07-09 13:59:48 +0200

    Allergen implementation started

--- src/app/models/dish.ts
+++ src/app/models/dish.ts
@@ -1,7 +1,9 @@
+import {Allergen} from './allergen';
+
 export interface Dish {
   id: string;
   name: string;
   description: string;
-  allergens: string;
+  allergens: Array<Allergen>;
   price: string;
 }
